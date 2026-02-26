8fb15ad49478aaf2e273378ab01e9d92841af0fd
SeBaS281203 <nsilvajosueseba@uss.edu.pe>
2026-02-26 02:26:02 -0500

    Crear ruta secreta para instalar BD

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,8 @@ require __DIR__ . '/public.php';
 require __DIR__ . '/cuenta.php';
 require __DIR__ . '/admin.php';
 require __DIR__ . '/organizer.php';
+
+Route::get('/instalar-bd', function () {
+    \Illuminate\Support\Facades\DB::unprepared(file_get_contents(base_path('mysql-init/01-schema.sql')));
+    return '¡Base de datos lista en la nube y tablas creadas!';
+});
