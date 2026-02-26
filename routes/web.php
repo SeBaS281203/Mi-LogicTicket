ae72ce6d8d6bf236f2715a9915d8a9e6bbd85659
SeBaS281203 <nsilvajosueseba@uss.edu.pe>
2026-02-26 09:46:17 -0500

    Apagar regla estricta de llave primaria en Aiven

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,18 @@ require __DIR__ . '/admin.php';
 require __DIR__ . '/organizer.php';
 
 Route::get('/instalar-bd', function () {
-    // 1. Borrar cualquier tabla o dato a medias
+    // 1. Borrar cualquier cosa a medias
     \Illuminate\Support\Facades\Artisan::call('db:wipe');
 
-    // 2. Borrar los procedimientos si ya existían
+    // 2. Borrar procedimientos anteriores
     \Illuminate\Support\Facades\DB::unprepared('DROP PROCEDURE IF EXISTS sp_approve_event;');
     \Illuminate\Support\Facades\DB::unprepared('DROP PROCEDURE IF EXISTS sp_dashboard_stats;');
     \Illuminate\Support\Facades\DB::unprepared('DROP PROCEDURE IF EXISTS sp_reject_event;');
     \Illuminate\Support\Facades\DB::unprepared('DROP PROCEDURE IF EXISTS sp_revenue_by_month;');
 
-    // 3. Ahora sí, instalar tu archivo SQL limpio
-    \Illuminate\Support\Facades\DB::unprepared(file_get_contents(base_path('mysql-init/01-schema.sql')));
+    // 3. APAGAR REGLA ESTRICTA DE AIVEN E INSTALAR TUS DATOS
+    $sql = "SET SESSION sql_require_primary_key = 0;\n" . file_get_contents(base_path('mysql-init/01-schema.sql'));
+    \Illuminate\Support\Facades\DB::unprepared($sql);
 
-    return '¡Limpieza profunda terminada y base de datos instalada con éxito!';
+    return '¡VICTORIA TOTAL! Base de datos instalada con éxito.';
 });
